e079ac52c4aa1013a2ef56c402f6c1d8d6a6688e
lore <lore.ferrario@protonmail.com>
2025-04-21 13:14:37 +0200

    fix header

--- components/header.js
+++ components/header.js
@@ -5,29 +5,27 @@ import HomeIcon from "@mui/icons-material/Home"
 export function Header({ children }) {
   return (
     <div className={styles.header}>
-      <div className={styles.headerLeftColumn}>
-        <a href="/">
-          <HomeIcon sx={{ color: "#fff", fontSize: 50 }} />{" "}
-        </a>
-      </div>
+      <div className={styles.headerLeftColumn}></div>
       <div className={styles.headerCenterColumn}>
-        <Image
-          src="/EDDY.png"
-          alt="awe Logo"
-          className={styles.aweLogo}
-          width={50}
-          height={50}
-          style={{ marginRight: "-10px" }}
-          priority
-        />
-        <Image
-          src="/eddy-logo.png"
-          alt="awe Logo"
-          className={styles.aweLogo}
-          width={75}
-          height={50}
-          priority
-        />
+        <a href="/">
+          <Image
+            src="/EDDY.png"
+            alt="awe Logo"
+            className={styles.aweLogo}
+            width={50}
+            height={50}
+            style={{ marginRight: "-10px" }}
+            priority
+          />
+          <Image
+            src="/eddy-logo.png"
+            alt="awe Logo"
+            className={styles.aweLogo}
+            width={75}
+            height={50}
+            priority
+          />
+        </a>
       </div>
       <div className={styles.headerRightColumn}></div>
     </div>
